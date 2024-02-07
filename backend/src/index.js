38d040a9e623c80e8a7aa6b8bb2b3a37d9531a73
PedroRoRi <ing.p.rodriguez4@gmail.com>
2024-02-07 18:52:18 -0500

    Cambio 3 video 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 async function main(){
     await app.listen(4000)
-    console.log('Servidor ejecutandose en el puerto 4000');
+    console.log('Servidor ejecutandose en puerto 4000');
 }
 
 main();
