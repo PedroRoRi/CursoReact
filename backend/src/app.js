8ea797ba7b5ba79e1351c9dcd9bf4f50b3b9385c
PedroRoRi <ing.p.rodriguez4@gmail.com>
2024-02-07 23:12:52 -0500

    Commit video 4

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 
+//configuracion
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//rutas
+app.get('/api/users', (req, res) => res.send('User Routes'));
+app.get('/api/notes', (req, res) => res.send('Notes Routes'));
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ const app = require('./app');
 require('./database');
 
 async function main(){
-    await app.listen(4000)
-    console.log('Servidor ejecutandose en el puerto 4000');
+    await app.listen(app.get('port'))
+    console.log('Servidor ejecutandose en el puerto ', app.get('port'));
 }
 
 main();
