1760162bfa33f749c12cba0127ff2ac5b58ea48d
PedroRoRi <ing.p.rodriguez4@gmail.com>
2024-02-08 22:16:10 -0500

    Commit video 13

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -1,9 +1,88 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateNote extends Component {
+  state = {
+    users: [],
+    userSelected: "",
+    date: new Date(),
+  };
+
+  componentDidMount = async () => {
+    const res = await axios.get("http://localhost:4000/api/users");
+    this.setState({ users: res.data.map((user) => user.username) });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  onInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onChangeDate = (date) => {
+    this.setState({ date });
+  };
+
   render() {
     return (
-      <div>CreateNote</div>
-    )
+      <div className="col-md-6 offset-md-3">
+        <div className="card card-body">
+          <h4>Crear una nota</h4>
+          {/** SELECT USER */}
+          <div className="form-group">
+            <select
+              className="form-control"
+              name="userSelected"
+              onChange={this.onInputChange}
+            >
+              {this.state.users.map((user) => (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="titulo"
+              name="titulo"
+              onChange={this.onInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              name="descripcion"
+              className="form-control"
+              placeholder="descripcion"
+              onChange={this.onInputChange}
+              required
+            ></textarea>
+          </div>
+
+          <div className="form-group">
+            <DatePicker
+              className="form-control"
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            ></DatePicker>
+          </div>
+          <form onSubmit={this.onSubmit}>
+            <button type="submit" className="btn btn-primary">
+              Guardar
+            </button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
