8e55a1821aa5becf35a5a996022d4a15bfa9a903
PedroRoRi <ing.p.rodriguez4@gmail.com>
2024-02-08 21:35:15 -0500

    Commit video 11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import Navigation from './components/Navigation'
-import CreateNote from './components/CreateNote'
-import CreateUser from './components/CreateUser'
-import NotesList from './components/NotesList'
+import Navigation from "./components/Navigation";
+import CreateNote from "./components/CreateNote";
+import CreateUser from "./components/CreateUser";
+import NotesList from "./components/NotesList";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation/>
-      <Routes>
-        <Route path='/' element={<NotesList/>}/> //¿Component or element?
-        <Route path='/edit/:id' element={<CreateNote/>}/>
-        <Route path='/create' element={<CreateNote/>}/>
-        <Route path='/user' element={<CreateUser/>}/>
-        Hello mundo!!
-      </Routes>
+      <Navigation />
+      <div className="container p-4">
+        <Routes>
+          <Route path="/" exact element={<NotesList />} />
+          <Route path="/edit/:id" element={<CreateNote />} />
+          <Route path="/create" element={<CreateNote />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
